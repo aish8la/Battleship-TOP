5f25200e0e397904659ed98a9eb3efd7dca11a18
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-09 01:17:34 +0500

    test: add hitsTaken test case to ship class

--- src/app.test.js
+++ src/app.test.js
@@ -8,4 +8,14 @@ test("Ship Instance has length", () => {
     const shipObject = new gameClass.Ship(4);
     expect(shipObject.length).toBeDefined();
     expect(shipObject.length).toBe(4);
-})
+});
+
+test("Ship Instance has hitsTaken", () => {
+    const shipObject = new gameClass.Ship(4);
+    expect(shipObject.hitsTaken).toBeDefined();
+});
+
+test("Ship Instance hitsTaken is 0", () => {
+    const shipObject = new gameClass.Ship(4);
+    expect(shipObject.hitsTaken).toBe(0);
+});
