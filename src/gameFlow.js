bccf76df54fd87bd96a73d5a40777aaac05e0f21
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-16 19:11:06 +0500

    feat: add method to initialize players in gameState Object

--- src/gameFlow.js
+++ src/gameFlow.js
@@ -3,6 +3,11 @@ import { Gameboard } from './app.js';
 import { Player } from './app.js';
 import { PLAYER_TYPES } from './app.js';
 
+export const gameState = {
+    player1: null,
+    player2: null,
+}
+
 function initGameboard() {
     return new Gameboard(Ship);
 }
@@ -15,4 +20,14 @@ export function initHumanPlayer(humanName) {
 export function initComputerPlayer() {
     const gameBoard = initGameboard();
     return new Player(PLAYER_TYPES.COMPUTER, "Computer", gameBoard);
+}
+
+export function startSinglePlayerGame(player1Name = "Player 1") {
+    gameState.player1 = initHumanPlayer(player1Name);
+    gameState.player2 = initComputerPlayer();
+}
+
+export function start2PlayerGame(player1Name = "Player 1", player2Name = "Player 2") {
+    gameState.player1 = initHumanPlayer(player1Name);
+    gameState.player2 = initHumanPlayer(player2Name);
 }
