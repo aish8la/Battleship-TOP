b4ef5780d375e2b22649aeaa53f6167d3842606e
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-18 00:49:06 +0500

    feat: make computer trigger game board update after turn

--- src/gameFlow.js
+++ src/gameFlow.js
@@ -2,6 +2,7 @@ import { Ship } from './app.js';
 import { Gameboard } from './app.js';
 import { Player } from './app.js';
 import { PLAYER_TYPES } from './app.js';
+import { updateGameBoards } from './displayerController.js';
 
 export const gameState = {
     player1: null,
@@ -91,6 +92,7 @@ function computerTurn() {
     if(currentPlayer.type === PLAYER_TYPES.COMPUTER) {
         const coordinates = currentPlayer.computerAttack();
         attackEnemy(coordinates);
+        updateGameBoards();
     }
 }
 
