7a69fe8234f6c10d5399515e526f0c2b78e26ecd
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-17 23:50:54 +0500

    feat: add function to initialize event listeners

---
+++ src/UIController.js
@@ -0,0 +1,23 @@
+import * as gameFlow from "./gameFlow.js";
+import * as displayControllers from "./displayerController.js";
+
+const body = document.querySelector('body');
+
+export function initializeEventListeners() {
+    body.addEventListener('click', (e) => {
+        if(e.target.id === "single-player") {
+            gameFlow.startSinglePlayerGame();
+            displayControllers.updateDisplay();
+        }
+        if(e.target.id === "two-player") {
+            gameFlow.start2PlayerGame();
+            displayControllers.updateDisplay();
+        }
+    });
+    body.addEventListener('click', (e) => {
+        if(e.target.id === "randomize") {
+            gameFlow.randomizePlacement();
+            displayControllers.updatePlacement()
+        }
+    });
+}
