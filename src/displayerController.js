459d9e9e73c63f3d9233308fd539cb01e1b95d20
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-17 23:19:28 +0500

    feat: add function to update placement board

--- src/displayerController.js
+++ src/displayerController.js
@@ -22,6 +22,14 @@ function removeElement(selector) {
     }
 }
 
+function removeFromParent(parentSelector, target) {
+    const parent = document.querySelector(parentSelector);
+    const child = parent.querySelector(target);
+    if(child && parent) {
+        parent.removeChild(child);
+    }
+}
+
 export function renderHome() {
     removeElement(".wrapper");
     const wrapper = elementGenerator("div", {"class": "wrapper", "id": "home-screen"});
@@ -37,13 +45,16 @@ export function renderHome() {
     elements.body.appendChild(wrapper);
 }
 
-function renderBoard(maxSize) {
+function renderBoard(maxSize, data) {
     const [maxX, maxY] = maxSize;
     const gridCtn = elementGenerator("div", {"class": "grid-container"});
-
+    let classList = "grid data-column";
+    if(data.class) {
+        classList += " " + data.class;
+    }
     for(let x = 1; x <= maxX; x++) {
         for(let y = 1; y <= maxY; y++) {
-            const grid = elementGenerator("div", {"class": "grid", "data-column": x, "data-row": y});
+            const grid = elementGenerator("div", {"class": classList, "data-column": x, "data-row": y});
             gridCtn.appendChild(grid);
         }
     }
@@ -55,9 +66,9 @@ export function renderShipPlacement(data) {
     const wrapper = elementGenerator("div", {"class": "wrapper", "id": "game-board"});
     const messageBox = elementGenerator("div", {"id": "message-box"}, "This is a message box for notifications");
     const gameCtn = elementGenerator("div", {"class": "ship-placement", "id": "game-boards"});
-    const gameBoards = elementGenerator("div", {"class": "board-container", "id": "player-board"});
+    const gameBoards = elementGenerator("div", {"class": "board-container", "id": "ship-placement"});
     const screenTitle = elementGenerator("h3", {}, "Place Ship");
-    const board = renderBoard([10, 10]);
+    const board = renderBoard([10, 10], data);
 
     const menu = elementGenerator("div", {"id": "button-ctn"});
     const button1 = elementGenerator("button", {"id": "toggle-orientation"}, "Toggle Orientation");
@@ -104,13 +115,23 @@ export function renderGameScreen() {
     elements.body.appendChild(wrapper);
 }
 
+export function updateBoard(parentID, data) {
+    removeFromParent(parentID,".grid-container");
+    const parent = document.querySelector(parentID);
+    parent.appendChild(renderBoard([10, 10], data));
+}
+
+export function updatePlacement() {
+    const data = gameFlow.placementData();
+    updateBoard("#ship-placement", data);
+}
+
 export function initDisplay() {
     renderHome();
 }
 
 export function updateDisplay() {
     if(gameFlow.gameState.currentState === gameFlow.STATE.SHIP_PLACEMENT) {
-        const data = gameFlow.placementData();
-        renderShipPlacement(data);
+        renderShipPlacement({});
     }
 }
