4551c00581e71a0fceec5b9e3af5bd1341358281
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-17 22:27:31 +0500

    test: update test cases to account for changes in exposed data

--- src/gameFlow.test.js
+++ src/gameFlow.test.js
@@ -1,16 +1,15 @@
 import * as gameFlow from './gameFlow.js';
 
 test("Initialize Human Player", () => {
-    const humanPlayer1 = gameFlow.initHumanPlayer("Human1");
-    expect(humanPlayer1.playerName).toBe("Human1");
-    expect(humanPlayer1.gameBoard).toBeDefined();
+    gameFlow.startSinglePlayerGame("Human1");
+    expect(gameFlow.gameState.player1.playerName).toBe("Human1");
+    expect(gameFlow.gameState.player1).toBeDefined();
 });
 
 test("Two Players have two different gameBoards", () => {
-    const player1 = gameFlow.initHumanPlayer("Human1");
-    const player2 = gameFlow.initHumanPlayer("Human2");
-    expect(player1.playerName).toBe("Human1");
-    expect(player2.playerName).toBe("Human2");
-    expect(player1).not.toBe(player2);
-    expect(player1.gameBoard).not.toBe(player2.gameBoard);
+    gameFlow.start2PlayerGame("Human1", "Human2");
+    expect(gameFlow.gameState.player1.playerName).toBe("Human1");
+    expect(gameFlow.gameState.player2.playerName).toBe("Human2");
+    expect(gameFlow.gameState.player1).not.toBe(gameFlow.gameState.player2);
+    expect(gameFlow.gameState.player1.gameBoard).not.toBe(gameFlow.gameState.player2.gameBoard);
 });
