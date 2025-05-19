a7f47fd95c7d50b9c0f18ab37b7734b9e8a18e85
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-19 22:13:29 +0500

    feat: implement a pubsub

---
+++ src/pubsub.js
@@ -0,0 +1,21 @@
+class PubSub {
+    #events = new Map();
+
+    subscribe(eventName, handler) {
+        if(!this.#events.has(eventName)) {
+            this.#events.set(eventName, new Set());
+        }
+
+        this.#events.get(eventName).add(handler);
+    }
+
+    unsubscribe(eventName, handler) {
+        this.#events.get(eventName)?.delete(handler);
+    }
+
+    publish(eventName, data) {
+        this.#events.get(eventName)?.forEach(handler => handler(data));
+    }
+}
+
+export const pubsub = new PubSub();
